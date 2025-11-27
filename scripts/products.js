51a134a3e1d9647ac935e2e5317a39f10e76c4ea
mMartinez00 <martinez.mauricio.0093@gmail.com>
2025-11-26 22:12:08 -0500

    feat(product) add size selector option to product__card if products category are hoodie & tshirt

--- scripts/products.js
+++ scripts/products.js
@@ -17,9 +17,15 @@ async function fetchProducts() {
         console.log(error)
     }
 
-} 
+}
+
+function setFilters(data) {
+    // const categories = 
+    // console.log(data)
+}
 
 function loadProducts(data) {
+    const filters = document.querySelectorAll
 
    const productsCard = data.map((product) => {
         return `
@@ -32,12 +38,31 @@ function loadProducts(data) {
                 <p class="products__card-description">${product.description}</p>
                 <div class="products__card-footer">
                     <p class="products__card-price">${product.price}</p>
+                    ${["hoodie", "tshirt"].includes(product.category) ? `
+                        <div class="products__card-sizes-group">
+                            <label for="sizes" class="products__card-sizes-label">Sizes:</label>
+
+                            <select 
+                                name="sizes" 
+                                id="sizes" 
+                                class="products__card-sizes-select"
+                            >
+                                <option value="s">S</option>
+                                <option value="m">M</option>
+                                <option value="l">L</option>
+                                <option value="xl">XL</option>
+                            </select>
+                        </div>
+` : ""}
                     <button class="products__card-btn">Add to Cart</button>
                 </div>
             </div>
         </article>
     `
     })
+
+    // setFilters(data)
+    
 
     productsGrid.innerHTML = productsCard.join("")
 
