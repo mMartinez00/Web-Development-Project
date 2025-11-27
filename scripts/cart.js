15d05859d9fc5370cefa1ca70be31d6b6a48aa1d
mMartinez00 <martinez.mauricio.0093@gmail.com>
2025-11-27 17:47:12 -0500

    featt(products): cartt.js now gets, saves, and adds to cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,17 +1,42 @@
+const cartKey = "cart"
+
+// Get cart from localStorage or empty array if nothing
+function getCart() {
+    const cartJSON = localStorage.getItem(cartKey)
+
+    return cartJSON ? JSON.parse(cartJSON) : [];
+}
+
+// Save cart array back to localStorage
+function saveCart(cart) {
+    localStorage.setItem(cartKey, JSON.stringify(cart))
+}
+
+// Export cart to different pages
+export function getCartItems() {
+    return getCart();
+}
+
 export function addToCart(productID, selectedSize) {
     const size = selectedSize === null ? "One Size" : selectedSize
 
-    let item = {
-        id: productID,
-        size,
+    const cart = getCart();
+
+    const existingItem = cart.find(
+        (item) => item.id === productID && item.size === size
+    );
+
+    if(existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            id: productID,
+            size: size,
+            quantity: 1
+        })
     }
 
-    const cart = JSON.parse(localStorage.getItem("cart")) || []
-
-    cart.push(item)
-
-
-    localStorage.setItem("cart", JSON.stringify(cart))
+    saveCart(cart)
 
     console.log(localStorage)
 
