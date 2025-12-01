62f667433bb5bf627b7457e16c55aec18627b678
mMartinez00 <martinez.mauricio.0093@gmail.com>
2025-12-01 14:14:54 -0500

    feat(navbar): integrate shared cart logic and dynamically show cart icon
    - Converted global.js to an ES module and added <script type='module'> to all relevant pages
    
    - Imported getCartItems() from cart.js to reuse centralized cart logic
    
    - Added insertCart() function to display cart icon in navbar when items exist
    
    - Prevented duplicate cart icon with .navbar__item--cart check
    
    - Refactored DOM creation to use createElement + appendChild for safer rendering

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,23 @@
+import { getCartItems } from "./cart.js";
+
 const navbarMenu = document.querySelector(".navbar__menu")
 
-console.log(navbarMenu)
+function insertCart() {
+    if(!navbarMenu) return;
+    
+    const cart = getCartItems()
+
+    if(cart.length > 0 && !navbarMenu.querySelector("navbar__item--cart")) {
+        const li = document.createElement("li")
+        li.classList.add("navbar__item", "navbar__item--cart")
+        li.innerHTML =  `<a class="navbar__link" href="./cart.html"><i class="fa-solid fa-cart-arrow-down"></i></a>`
+        
+
+        navbarMenu.appendChild(li)
+    }
+  
+
+    console.log(cart)
+}
+
+insertCart()
