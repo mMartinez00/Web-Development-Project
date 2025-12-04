0883758d035587ea2b912f2b81b8dee334688531
mMartinez00 <martinez.mauricio.0093@gmail.com>
2025-12-04 10:50:17 -0500

    revise(blogs): format blog dates and clean up blog template structure:   - Added Intl.DateTimeFormat for consistent date formatting   - Parsed ISO date strings into JS Date objects   - Updated blog template markup and indentation   - Replaced raw blog.date with formattedDate

--- scripts/blog.js
+++ scripts/blog.js
@@ -19,27 +19,38 @@ async function fetchBlogs() {
 }
 
 function loadBlogs(allBlogs) {
+
     const blogs = allBlogs.map((blog) => {
+
+      const date = new Date(blog.date)
+  
+      const formattedDate = new Intl.DateTimeFormat('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: '2-digit',
+      }).format(date)
+
+
         return `
-    <div class="blog__item">
-      <div class="blog__image-box">
-        <img class="blog__image" 
-             src="assets/images/falcons_logo.png"
-             alt="falcons logo">
-      </div>
+            <div class="blog__item">
+              <div class="blog__image-box">
+                <img class="blog__image" 
+                     src="assets/images/falcons_logo.png"
+                     alt="falcons logo">
+              </div>
 
-      <div class="blog__content">
-        <div class="blog__header">
-          <h2 class="blog__title">${blog.title}</h2>
-          <p class="blog__date"><strong>${blog.date}</p>
-        </div>
+                      <div class="blog__content">
+                <div class="blog__header">
+                  <h2 class="blog__title">${blog.title}</h2>
+                  <p class="blog__date"><strong>${formattedDate}</p>
+                </div>
 
-        <p class="blog__text">
-          ${blog.text}
-        </p>
-      </div>
-    </div>
-    `
+                        <p class="blog__text">
+                  ${blog.text}
+                </p>
+              </div>
+            </div>
+            `
     }) 
 
     blogsSection.innerHTML = blogs.join("")
